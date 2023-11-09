a01d04cae9bb3d6b78b070de432fc4658d9a4cf0
Tehaunui Shing Soi <150084144+znxtgen@users.noreply.github.com>
2023-11-09 16:24:30 +1300

    Array challenge

--- loop.php
+++ loop.php
@@ -11,3 +11,21 @@ $articles = [
 foreach ($articles as $index => $article) {
     echo "<br/> $index - $article" . ", ";
 }
+
+
+/*
+Challenge - Arrays
+
+Create a variable called array that contains an array with three simple values.
+These values can be whatever you like.
+Explicitly assign the string index of "third" to the third element.
+Write a foreach  loop that contains the following statement to print out
+each element of the array, along with its index:
+echo "$index = $value."; 
+*/
+
+$array = ["Amy", "Steven", "third" => "Gary"];
+
+foreach ($array as $index => $value) {
+    echo "<br/>$index = $value.";
+}
